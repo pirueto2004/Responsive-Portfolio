32fdbe9d1041d2228d02e68b81b3ae20735642e4
Julio Bell <juliobell2014@gmail.com>
2019-05-10 21:40:11 -0400

    Updated and redesigned the portfolio

---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $("#vms").on("click", function(){
+        window.location.href = "https://iamjpyo.github.io/group_project/";
+    });
+    $("#hang-man").on("click", function(){
+        window.location.href = "https://pirueto2004.github.io/Word-Guess-Game/";
+    });
+    $("#trivia").on("click", function(){
+        window.location.href = "https://pirueto2004.github.io/TriviaGame/";
+    });
+    $("#train").on("click", function(){
+        window.location.href = "https://pirueto2004.github.io/TrainScheduler/";
+    });
+    $("#gifs").on("click", function(){
+        window.location.href = "https://pirueto2004.github.io/GifBoard/";
+    });
+    $("#crystal").on("click", function(){
+        window.location.href = "https://pirueto2004.github.io/unit-4-game/";
+    });
+
+});
